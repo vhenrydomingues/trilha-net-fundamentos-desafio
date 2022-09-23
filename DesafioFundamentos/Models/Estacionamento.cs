b3f62b2b37192a5dbf9301aef1d686091014e3d4
HENRY <vhenrydomingues@gmail.com>
2022-09-22 22:39:35 -0300

    alteração para deixar a tela do console com novos dados e opções

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -65,17 +65,64 @@ namespace DesafioFundamentos.Models
             // Verifica se há veículos no estacionamento
             if (veiculos.Any())
             {
+
                 Console.WriteLine("Os veículos estacionados são:");
                 // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
                 foreach (string item in veiculos)
                 {
-                    Console.WriteLine(item);
+                    Console.WriteLine($"Placa: {item}");
                 }
             }
             else
             {
                 Console.WriteLine("Não há veículos estacionados.");
             }
+        }
+
+        public void TabelaPreco()
+        {
+
+            Console.Clear();
+            Console.WriteLine("_________________________________________________________________________");
+            Console.WriteLine("|                      TABELA DE PRECO                                  |");
+            Console.WriteLine("_________________________________________________________________________");
+
+            Console.WriteLine($"Valor para entrada: {precoInicial}");
+            Console.WriteLine($"Valor da Hora: {precoPorHora}");
+
+            Console.WriteLine("\nDigite:\n 1 - Alterar tabela de valores\n 2 - Voltar ao menu principal");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    Atualizar();
+                    break;
+
+                case "2":
+                    break;
+
+                default:
+                    Console.Clear();
+                    Console.WriteLine("Por favor digite uma opção válida...");
+                    Console.WriteLine("Pressione uma tecla para continuar...");
+                    Console.ReadLine();
+                    TabelaPreco();
+                    break;
+            }
+
+        }
+
+        public void Atualizar()
+        {
+            Console.Clear();
+            Console.WriteLine("_________________________________________________________________________");
+            Console.WriteLine("|                          ATUALIZANDO VALORES                          |");
+            Console.WriteLine("_________________________________________________________________________");
+
+            Console.WriteLine($"O Valor para entrada atual é {precoInicial} \nDigite o novo valor:");
+            precoInicial = decimal.Parse(Console.ReadLine());
+            Console.WriteLine($"O Valor da hora atual é {precoPorHora} \nDigite o novo valor:");
+            precoPorHora = decimal.Parse(Console.ReadLine());
         }
     }
 }
--- DesafioFundamentos/Program.cs
+++ DesafioFundamentos/Program.cs
@@ -3,31 +3,35 @@
 // Coloca o encoding para UTF8 para exibir acentuação
 Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-decimal precoInicial = 0;
-decimal precoPorHora = 0;
+decimal precoInicial = 5;
+decimal precoPorHora = 10;
 
-Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\n" +
-                  "Digite o preço inicial:");
-precoInicial = Convert.ToDecimal(Console.ReadLine());
 
-Console.WriteLine("Agora digite o preço por hora:");
-precoPorHora = Convert.ToDecimal(Console.ReadLine());
-
+Console.WriteLine("_________________________________________________________");
+Console.WriteLine("|                SEJA BEM VINDO!                        |");
+Console.WriteLine("|            SISTEMA DE ESTACIONAMENTO                  |");
+Console.WriteLine("_________________________________________________________");
 // Instancia a classe Estacionamento, já com os valores obtidos anteriormente
 Estacionamento es = new Estacionamento(precoInicial, precoPorHora);
 
 string opcao = string.Empty;
 bool exibirMenu = true;
 
+Console.WriteLine("Pressione uma tecla para continuar...");
+Console.ReadLine();
 // Realiza o loop do menu
 while (exibirMenu)
 {
     Console.Clear();
+    Console.WriteLine("_________________________________________________________");
+    Console.WriteLine("|                    MENU PRINCIPAL                     |");
+    Console.WriteLine("_________________________________________________________");
     Console.WriteLine("Digite a sua opção:");
     Console.WriteLine("1 - Cadastrar veículo");
     Console.WriteLine("2 - Remover veículo");
     Console.WriteLine("3 - Listar veículos");
-    Console.WriteLine("4 - Encerrar");
+    Console.WriteLine("4 - Consultar tabela de preço");
+    Console.WriteLine("5 - Encerrar o sistema");
 
     switch (Console.ReadLine())
     {
@@ -42,8 +46,11 @@ while (exibirMenu)
         case "3":
             es.ListarVeiculos();
             break;
-
         case "4":
+            es.TabelaPreco();
+            break;
+
+        case "5":
             exibirMenu = false;
             break;
 
@@ -52,8 +59,9 @@ while (exibirMenu)
             break;
     }
 
-    Console.WriteLine("Pressione uma tecla para continuar");
+    Console.WriteLine("Pressione uma tecla para continuar...");
     Console.ReadLine();
 }
 
+Console.WriteLine("Obrigado por utilizar o nosso sistema, até a proxima!");
 Console.WriteLine("O programa se encerrou");
